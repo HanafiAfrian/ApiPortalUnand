fc6181263344ed07e9725ad93cee624acbc56188
“HanafiAfrian” <hanafiafrian611@gmail.com>
2023-08-29 09:49:32 +0700

    ok

--- src/routes/route-mhs.js
+++ src/routes/route-mhs.js
@@ -1,9 +1,5 @@
 const router = require('express').Router();
 const { mhs } = require('../controllers');
-
-
 router.get('/mhs', mhs.getDataMhs);
-
-
 router.get('/mhs/:id', mhs.getDataMhsByID);
 module.exports = router;
